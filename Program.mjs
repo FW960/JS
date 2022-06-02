330e5da5b808447114b51654fc90dcb01a420091
FW960 <artuninp@gmail.com>
2022-06-03 03:37:08 +0800

    Корзина и подсчет её стоимости

--- Program.mjs
+++ Program.mjs
@@ -1,4 +1,20 @@
 "use strict";
 import _ from "lodash";
 
-console.log(_.random(0, 100));
+let cart =
+{
+    goods: [100, 200, 300, 600],
+
+    countBasketPrice()
+    {
+        let wholePrice = 0;
+
+        for (let i = 0; i < this.goods.length; i++)
+            wholePrice += this.goods[i];
+            
+        return wholePrice;
+    }
+
+}
+
+console.log(cart.countBasketPrice());
