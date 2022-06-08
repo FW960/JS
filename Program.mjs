5588ad66bd3751031c914d65a5725771796a735f
FW960 <artuninp@gmail.com>
2022-06-09 02:00:57 +0800

    Подсчет сотен, десятков и сотен

--- Program.mjs
+++ Program.mjs
@@ -1,3 +1,21 @@
 "use strict";
-import _ from "./lodash1.js";
+import _ from "lodash";
+
+function crObj(num)
+{
+    let num1 = _.toString(num);
+
+    if (num1.length >= 4)
+        return null;
+
+        let toReturnObj  = {units: parseInt(num1[0]), dozens: parseInt(num1[1]), hundreds: parseInt(num1[2])};
+        
+        return toReturnObj;
+
+}
+
+let obj = crObj(123);
+
+console.log(obj);
+
 
